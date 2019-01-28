17edfde77290907ecb00bf671ff3d1ca3e959bc3
D-madhukar <madhukarpateld@gmail.com>
2019-01-28 17:41:18 +0530

    HeadersDemo is modified

--- Servlets & Jsp_Assignment/WebProject1/src/com/servlet/HeadersDemo.java
+++ Servlets & Jsp_Assignment/WebProject1/src/com/servlet/HeadersDemo.java
@@ -16,18 +16,9 @@ import javax.servlet.http.HttpServletResponse;
 //@WebServlet("/HeadersDemo")
 public class HeadersDemo extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public HeadersDemo() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+    	public HeadersDemo() {
+        	super();
+    	}
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		PrintWriter out=response.getWriter();
 		Enumeration<String> headers=request.getHeaderNames();
